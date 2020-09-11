d83f6f932b8e7bfaa497652ed8e1eea5ca9e083d
PravinSelva5 <selvarap@mcmaster.ca>
2020-09-11 12:58:47 -0400

    worked on cafe order checker question, needed to use site's solution

--- Array_and_String_Manipulation/CafeOrderChecker.py
+++ Array_and_String_Manipulation/CafeOrderChecker.py
@@ -14,36 +14,79 @@ Given all three lists, write a function to check that my service is first-come, 
 
 We'll represent each customer order as a unique integer.
 
+RECURSIVE SOLUTION FROM WEBSITE
 '''
 
-def is_first_come_first_served(take_out_orders, dine_in_orders, served_orders):
-    
-    
-    # Initialize values
-    total_orders = len(served_orders)
-    #current_take_out_index = 0
-    #current_dine_in_index = 0
-    current_served_orders_index = 0
-    
-    while current_served_orders_index < total_orders:
-      print(current_served_orders_index)
-      if (served_orders[current_served_orders_index] == take_out_orders[0]) and (not take_out_orders):
-        current_served_orders_index += 1
-        del take_out_orders[0]
-        print(take_out_orders)
-        
-      elif (served_orders[current_served_orders_index] == dine_in_orders[0]) and (not dine_in_orders):
-        current_served_orders_index += 1
-        del dine_in_orders[0]
-        print(dine_in_orders)
-      else:
-        continue
-    
-    if current_served_orders_index == total_orders:
-      return True
-    else:
-      return False
+import unittest
 
-output1 = is_first_come_first_served([17, 8, 24],[12, 19, 2],[17, 8, 12, 19, 24, 2])
-print(output1)
+def is_first_come_first_served(take_out_orders, dine_in_orders, served_orders, take_out_orders_index=0, dine_in_orders_index=0,
+                               served_orders_index=0):
+    # Base case we've hit the end of served_orders
+    if served_orders_index == len(served_orders):
+        return True
+
+    # If we still have orders in take_out_orders
+    # and the current order in take_out_orders is the same
+    # as the current order in served_orders
+    if ((take_out_orders_index < len(take_out_orders)) and
+            take_out_orders[take_out_orders_index] == served_orders[served_orders_index]):
+        take_out_orders_index += 1
+
+    # If we still have orders in dine_in_orders
+    # and the current order in dine_in_orders is the same
+    # as the current order in served_orders
+    elif ((dine_in_orders_index < len(dine_in_orders)) and
+            dine_in_orders[dine_in_orders_index] == served_orders[served_orders_index]):
+        dine_in_orders_index += 1
+
+    # If the current order in served_orders doesn't match
+    # the current order in take_out_orders or dine_in_orders, then we're not
+    # serving in first-come, first-served order.
+    else:
+        return False
+
+    # The current order in served_orders has now been "accounted for"
+    # so move on to the next one
+    served_orders_index += 1
+    return is_first_come_first_served(
+        take_out_orders, dine_in_orders, served_orders,
+        take_out_orders_index, dine_in_orders_index, served_orders_index)
+
+
+# Tests
+class Test(unittest.TestCase):
+
+    def test_both_registers_have_same_number_of_orders(self):
+        result = is_first_come_first_served([1, 4, 5], [2, 3, 6], [1, 2, 3, 4, 5, 6])
+        self.assertTrue(result)
+
+    def test_registers_have_different_lengths(self):
+        result = is_first_come_first_served([1, 5], [2, 3, 6], [1, 2, 6, 3, 5])
+        self.assertFalse(result)
+
+    def test_one_register_is_empty(self):
+        result = is_first_come_first_served([], [2, 3, 6], [2, 3, 6])
+        self.assertTrue(result)
+
+    def test_served_orders_is_missing_orders(self):
+        result = is_first_come_first_served([1, 5], [2, 3, 6], [1, 6, 3, 5])
+        self.assertFalse(result)
+
+    def test_served_orders_has_extra_orders(self):
+        result = is_first_come_first_served([1, 5], [2, 3, 6], [1, 2, 3, 5, 6, 8])
+        self.assertFalse(result)
+
+    def test_one_register_has_extra_orders(self):
+        result = is_first_come_first_served([1, 9], [7, 8], [1, 7, 8])
+        self.assertFalse(result)
+
+    def test_one_register_has_unserved_orders(self):
+        result = is_first_come_first_served([55, 9], [7, 8], [1, 7, 8, 9])
+        self.assertFalse(result)
+
+    def test_order_numbers_are_not_sequential(self):
+        result = is_first_come_first_served([27, 12, 18], [55, 31, 8], [55, 31, 8, 27, 12, 18])
+        self.assertTrue(result)
+
+unittest.main(verbosity=2)
 
