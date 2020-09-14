c89773f72797203b21fdfe4b46df808a40da8e2b
PravinSelva5 <selvarap@mcmaster.ca>
2020-09-14 15:31:57 -0400

    Updated folder organization

--- Hashing_and_Hashing_Tables/Palindrome_Permutation.py
+++ Hashing_and_Hashing_Tables/Palindrome_Permutation.py
@@ -49,6 +49,25 @@ def has_palindrome_permutation(the_string):
     else:
         return False
 
+
+'''
+A BETTER SOLUTION THAT LEVERAGES SETS RATHER THAN A DICTIONARY
+
+  def has_palindrome_permutation(the_string):
+        # Track characters we've seen an odd number of times
+    unpaired_characters = set()
+
+    for char in the_string:
+        if char in unpaired_characters:
+            unpaired_characters.remove(char)
+        else:
+            unpaired_characters.add(char)
+
+    # The string has a palindrome permutation if it
+    # has one or zero characters without a pair
+    return len(unpaired_characters) <= 1
+'''
+
 # Tests
 
 class Test(unittest.TestCase):
