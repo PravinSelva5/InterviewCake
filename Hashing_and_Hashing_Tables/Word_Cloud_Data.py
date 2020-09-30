572ba7de07240ef8170ebdc76bdf71520e3ff108
PravinSelva5 <selvarap@mcmaster.ca>
2020-09-30 10:03:10 -0400

    word cloud data, tough question, need to review again later on

--- Hashing_and_Hashing_Tables/Word_Cloud_Data.py
+++ Hashing_and_Hashing_Tables/Word_Cloud_Data.py
@@ -15,8 +15,9 @@ Make reasonable (not necessarily perfect) decisions about cases like "After" and
 
 NEED TO LEVERAGE ARRAY SPLICING 
 
-'''
-
+------------
+ ATTEMPT 1
+------------
 
         # Count the frequency of each word
         
@@ -48,11 +49,9 @@ NEED TO LEVERAGE ARRAY SPLICING
                 
                 temp_string = ''
 
-
-'''
-ATTEMPT TWO
-'''
-
+--------------
+ ATTEMPT TWO
+--------------
 
  # List of given input_string
         self.list_input_string = input_string.split()
@@ -79,3 +78,157 @@ ATTEMPT TWO
                 self.words_to_counts[word] += 1
         
         print(self.words_to_counts)
+
+'''
+
+import unittest
+
+
+class WordCloudData(object):
+
+    def __init__(self, input_string):
+        self.words_to_counts = {}
+        self.populate_words_to_counts(input_string)
+    
+    def populate_words_to_counts(self, input_string):
+        
+        current_word_start_index = 0
+        current_word_length = 0
+        
+        for i, character in enumerate(input_string):
+            
+            if i == len(input_string) - 1:
+                if character.isalpha():
+                    current_word_length += 1
+                if current_word_length > 0:
+                    current_word = input_string[current_word_start_index : current_word_start_index + current_word_length]
+                    self.add_word_to_dictionary(current_word)
+        
+            # if we reach a space or emdash we know we're at the end of a word, so we add it to the dictionary & reset our current word
+            elif character == ' ' or character == '\u2014':
+                if current_word_length > 0:
+                    current_word = input_string[current_word_start_index : current_word_start_index + current_word_length]
+                    self.add_word_to_dictionary(current_word)
+                    current_word_length = 0
+            
+            # Split on ellipses, so if we get two periods in a row we add the curernt word to our dictionary and reset our current word
+            elif character == '.':
+                if i < len(input_string) - 1 and input_string[i+1] == '.':
+                    if current_word_length > 0:
+                        current_word = input_string[current_word_start_index : current_word_start_index + current_word_length]
+                        self.add_word_to_dictionary(current_word)
+                        current_word_length = 0
+            
+            # If the character is a letter or an apostrophe, we add it to our current word
+            elif character.isalpha() or character == '\'':
+                if current_word_length == 0:
+                    current_word_start_index = i
+                current_word_length += 1
+            
+            
+            # If the character is a hyphen, we want to check if it's surrounded by letters
+            elif character == '-':
+                if i > 0 and input_string[i-1].isalpha() and input_string[i+1].isalpha():
+                    
+                    if current_word_length == 0:
+                        current_word_start_index = i
+                    current_word_length += 1
+                else:
+                    if current_word_length > 0:
+                        current_word = input_string[current_word_start_index : current_word_start_index + current_word_length]
+                        self.add_word_to_dictionary(current_word)
+                        current_word_length = 0
+    
+    def add_word_to_dictionary(self, word):
+        
+        if word in self.words_to_counts:
+            self.words_to_counts[word] += 1
+        
+        # If a lowercase version is in the dictionary, we know our input word must be uppercase
+        # but we only include uppercase words if they're always uppercase
+        # so we just increment the lowercase version's count
+        elif word.lower() in self.words_to_counts:
+            self.words_to_counts[word.lower()] += 1
+        
+        
+        elif word.capitalize() in self.words_to_counts:
+            self.words_to_counts[word] = 1
+            self.words_to_counts[word] += self.words_to_counts[word.capitalize()]
+            del self.words_to_counts[word.capitalize()]
+        
+        
+        # Otherwise the word is not in the dictionary
+        else:
+            self.words_to_counts[word] = 1
+# Tests 
+
+# There are lots of valid solutions for this one. You
+# might have to edit some of these tests if you made
+# different design decisions in your solution.
+
+class Test(unittest.TestCase):
+
+    def test_simple_sentence(self):
+        input = 'I like cake'
+
+        word_cloud = WordCloudData(input)
+        actual = word_cloud.words_to_counts
+
+        expected = {'I': 1, 'like': 1, 'cake': 1}
+        self.assertEqual(actual, expected)
+
+    def test_longer_sentence(self):
+        input = 'Chocolate cake for dinner and pound cake for dessert'
+
+        word_cloud = WordCloudData(input)
+        actual = word_cloud.words_to_counts
+
+        expected = {
+            'and': 1,
+            'pound': 1,
+            'for': 2,
+            'dessert': 1,
+            'Chocolate': 1,
+            'dinner': 1,
+            'cake': 2,
+        }
+        self.assertEqual(actual, expected)
+
+    def test_punctuation(self):
+        input = 'Strawberry short cake? Yum!'
+
+        word_cloud = WordCloudData(input)
+        actual = word_cloud.words_to_counts
+
+        expected = {'cake': 1, 'Strawberry': 1, 'short': 1, 'Yum': 1}
+        self.assertEqual(actual, expected)
+
+    def test_hyphenated_words(self):
+        input = 'Dessert - mille-feuille cake'
+
+        word_cloud = WordCloudData(input)
+        actual = word_cloud.words_to_counts
+
+        expected = {'cake': 1, 'Dessert': 1, 'mille-feuille': 1}
+        self.assertEqual(actual, expected)
+
+    def test_ellipses_between_words(self):
+        input = 'Mmm...mmm...decisions...decisions'
+
+        word_cloud = WordCloudData(input)
+        actual = word_cloud.words_to_counts
+
+        expected = {'mmm': 2, 'decisions': 2}
+        self.assertEqual(actual, expected)
+
+    def test_apostrophes(self):
+        input = "Allie's Bakery: Sasha's Cakes"
+
+        word_cloud = WordCloudData(input)
+        actual = word_cloud.words_to_counts
+
+        expected = {"Bakery": 1, "Cakes": 1, "Allie's": 1, "Sasha's": 1}
+        self.assertEqual(actual, expected)
+
+
+unittest.main(verbosity=2)
